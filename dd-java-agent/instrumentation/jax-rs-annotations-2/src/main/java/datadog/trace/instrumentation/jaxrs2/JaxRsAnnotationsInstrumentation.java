9756c1b5a5c8ecd79b765796964d6338c3e5389e
Gustavo Lopes <mail@geleia.net>
2022-04-21 14:32:40 +0100

    Intercept jax-rs path parameters

--- dd-java-agent/instrumentation/jax-rs-annotations-2/src/main/java/datadog/trace/instrumentation/jaxrs2/JaxRsAnnotationsInstrumentation.java
+++ dd-java-agent/instrumentation/jax-rs-annotations-2/src/main/java/datadog/trace/instrumentation/jaxrs2/JaxRsAnnotationsInstrumentation.java
@@ -60,6 +60,7 @@ public final class JaxRsAnnotationsInstrumentation extends Instrumenter.Tracing
       "datadog.trace.agent.tooling.ClassHierarchyIterable",
       "datadog.trace.agent.tooling.ClassHierarchyIterable$ClassIterator",
       packageName + ".JaxRsAnnotationsDecorator",
+      packageName + ".JaxRsAnnotationsDecorator$MethodDetails",
     };
   }
 
@@ -112,7 +113,7 @@ public final class JaxRsAnnotationsInstrumentation extends Instrumenter.Tracing
 
       final AgentSpan span = startSpan(JAX_ENDPOINT_OPERATION_NAME);
       span.setMeasured(true);
-      DECORATE.onJaxRsSpan(span, parent, target.getClass(), method);
+      DECORATE.onJaxRsSpan(span, parent, target.getClass(), method, args);
       DECORATE.afterStart(span);
 
       final AgentScope scope = activateSpan(span);
